8d5976628e6922274f63fdbdb69fdb0048b82ad1
Lam Pham-Sy <lam.pham-sy@scality.com>
2016-10-11 16:33:26 +0200

    Update bash file for running encoding benchmarks

--- Examples/encode_decode.sh
+++ Examples/encode_decode.sh
@@ -1,21 +1,41 @@
 #!/bin/bash -e
-#
-# Copyright (C) 2014 Red Hat <contact@redhat.com>
-#
-# Author: Loic Dachary <loic@dachary.org>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Library Public License as published by
-# the Free Software Foundation; either version 2, or (at your option)
-# any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Library Public License for more details.
-#
-trap "rm -fr T Coding"  EXIT
 
-dd if=/dev/urandom of=T bs=4096 count=1
-./encoder T 3 2 reed_sol_van 8 0  0
-./decoder T
+trap "rm -fr T Coding"  EXIT
+KB=1024
+MB=$KB*$KB
+
+k=12
+m=6
+w=8
+n=1000
+pk_size=$MB
+
+s1=10*$MB
+s2=2
+dd if=/dev/urandom of=T bs=$s1 count=$s2
+
+echo "*-----------PARAMETERS--------------*"
+echo "Erasure codes params: k = $k, m = $m"
+echo "Galois field over GF(2^$w)"
+echo "Object size: $(($s1*$s2/($MB))) (MB)"
+echo "Number of encodings: $n"
+
+echo "*-----------ENCODING USING GF MATRIX----------------*"
+echo "Reed-Solomon"
+./encoder T $k $m reed_sol_van $w 0 0 $n
+
+echo "reed_sol_r6_op"
+./encoder T $k $m reed_sol_van $w 0 0 $n
+
+echo "OUR CODE"
+./encoder T $k $m ec_pcm $w 0 0 $n
+
+echo "*-----------ENCODING USING BINARY MATRIX----------------*"
+echo "cauchy_orig"
+./encoder T $k $m cauchy_orig $w $pk_size  0 $n
+
+echo "cauchy_good"
+./encoder T $k $m cauchy_good $w $pk_size  0 $n
+
+echo "OUR CODE"
+./encoder T $k $m ec_pcm $w $pk_size  0 $n cauchy
